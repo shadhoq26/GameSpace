6845cbaa54cc001dc501e6062ba778e7159ee853
shadhoq26 <shadhoque26@outlook.com>
2023-04-16 05:47:43 +0100

    contact us page completion

--- ContactUs.php
+++ ContactUs.php
@@ -5,9 +5,9 @@
  <!-- This is where the text for the About page will be entered as well as a image below it.  -->
  <div id="textContainer2">
             <div id="textInnerContainer2">
-                <h1> About Us </h1> <br>
+                <h1> WHAT IS GAMESPACE ? </h1> <br>
                 <p> 
-                I am aiming to create a game review website where people can 
+                GameSpace is a game review website where people can 
                 rank games based on their own enjoyment. You will be able to rank 
                 videogames as well as be able to write reviews on games. 
                 Furthermore, you can rate and view other people’s reviews making 
@@ -16,10 +16,20 @@
                 reviews of the games they have played. The aim is to create a 
                 social, friendly environment for users to share their opinions and 
                 comment on other user’s opinions on game of their choice.
+            </p>
+            <h2> Contact information </h2> <br>
+            <p> 
+                <span>Email:</span> GameSpace20202@outlook.com
+                <br>
+                <br>
+                <span>INSTAGRAM:</span> @GameSpace20
+                <br>
+                <span>TWITTER:</span> @GameSpace20
+                <br>
+                <span>FACEBOOK:</span> @GameSpace2002            
             </p> 
-            <br>
-            <p> ok</p>
-            <img id= "contactImage" src="resources/profilephoto.jpg">
+            <!-- <br>
+            <img id= "contactImage" src="resources/profilephoto.jpg"> -->
             </div>
         </div>
         <!-- background image for About page -->
