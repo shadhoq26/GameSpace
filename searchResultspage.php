f1539ca5e5637a3e8c42dc3b1c1f74eda608fd7f
shadhoq26 <shadhoque26@outlook.com>
2023-04-21 02:19:14 +0100

    css changes

--- searchResultspage.php
+++ searchResultspage.php
@@ -1,57 +1,60 @@
 <?php include_once "header.php"?>
 <link rel= "Stylesheet" type= "text/css" href="css/searchresults.css">
 
-<?php 
-include "database.php";
-include "average-score-script.php";
-if(!isset($_GET["search"])){
-    header("location:index.php");
-    exit();
-}
-$searchText= $_GET["search"];
-$searchSQL ="SELECT * FROM games WHERE Name LIKE '%$searchText%' OR 
-    Genre LIKE '%$searchText%' OR
-    Platform LIKE '%$searchText%' OR
-    Publisher LIKE '%$searchText%' "; 
- 
-$searchResults = $database->query($searchSQL);
-$searchResults = $searchResults->fetchAll();
-?>
-
-<div id="box-container2">
-    <?php
-    if(count($searchResults)>0 && !empty($searchText)){
-        for($i=0; $i<count($searchResults);$i++){
-        $url= "ViewGamepage.php?id=".$searchResults[$i]["game_ID"];
-        
-        $average= getAverage($searchResults[$i]["game_ID"]);
-        ?>
-        <div id="first-box2" onclick="location.href='<?php echo $url ?>'">
-            <div id="search-image-container">
-                <img src="resources/GameImages/<?php echo $searchResults[$i]["Cover_Image"] ?>"/>
-            </div>
-            <div id="information-box">
-                <h1 class = "boxText2"><?php echo $searchResults[$i]["Name"] ?></h1> 
-                <p><span>Publisher:</span> <?php echo $searchResults[$i]["Publisher"] ?></p>
-                <p><span>Platform:<span> <?php echo $searchResults[$i]["Platform"] ?></p>
-                <p><span>Price:</span> <?php echo $searchResults[$i]["Price"] ?></p>
-            </div>
-            <span class="divider"></span>
-            <div id="search-rating-box">   
-                <h1><?php echo $average;?></h1>
-            </div> 
-        </div>
-        <?php
-        }
-    }else{
-        ?>
-        <p id="searchresultstext"> No search results found, please try again.</p>
-        <?php
+<html>
+    <body>
+    <?php 
+    include "database.php";
+    include "average-score-script.php";
+    if(!isset($_GET["search"])){
+        header("location:index.php");
+        exit();
     }
-    ?> 
-</div>
-</div>
-<!-- <div id="search-background"> </div> -->
+    $searchText= $_GET["search"];
+    $searchSQL ="SELECT * FROM games WHERE Name LIKE '%$searchText%' OR 
+        Genre LIKE '%$searchText%' OR
+        Platform LIKE '%$searchText%' OR
+        Publisher LIKE '%$searchText%' "; 
+    
+    $searchResults = $database->query($searchSQL);
+    $searchResults = $searchResults->fetchAll();
+    ?>
+
+    <div id="box-container2">
+        <?php
+        if(count($searchResults)>0 && !empty($searchText)){
+            for($i=0; $i<count($searchResults);$i++){
+            $url= "ViewGamepage.php?id=".$searchResults[$i]["game_ID"];
+            
+            $average= getAverage($searchResults[$i]["game_ID"]);
+            ?>
+            <div id="first-box2" onclick="location.href='<?php echo $url ?>'">
+                <div id="search-image-container">
+                    <img src="resources/GameImages/<?php echo $searchResults[$i]["Cover_Image"] ?>"/>
+                </div>
+                <div id="information-box">
+                    <h1 class = "boxText2"><?php echo $searchResults[$i]["Name"] ?></h1> 
+                    <p><span>Publisher:</span> <?php echo $searchResults[$i]["Publisher"] ?></p>
+                    <p><span>Platform:<span> <?php echo $searchResults[$i]["Platform"] ?></p>
+                    <p><span>Price:</span> <?php echo $searchResults[$i]["Price"] ?></p>
+                </div>
+                <span class="divider"></span>
+                <div id="search-rating-box">   
+                    <h1><?php echo $average;?></h1>
+                </div> 
+            </div>
+            <?php
+            }
+        }else{
+            ?>
+            <p id="searchresultstext"> No search results found, please try again.</p>
+            <?php
+        }
+        ?> 
+    </div>
+    </div>
+    <!-- <div id="search-background"> </div> -->
+</html>
 
 
 
